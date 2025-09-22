9297d1f5a14e1b3ee9422e281496260976de3492
Juergen Schwitalla <juergen.schwitalla@eviden.com>
2025-09-22 07:43:59 +0200

    added copyright header to python files

--- src/quark_plugin_myqlm/mappings.py
+++ src/quark_plugin_myqlm/mappings.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2025 Science + Computing AG / Eviden SE (Atos Group)
+#
+# This file is part of the QUARK benchmarking framework.
+# It provides a wrapper for myQLM (https://myqlm.github.io/).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+# Contact: stefan-raimund.maschek@eviden.com
+
+
 from dataclasses import dataclass
 
 from quark.core import Core, Result, Data
